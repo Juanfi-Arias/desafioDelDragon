b548845b8ba1c8866a6cce0475cd535a7da61b3e
Juan Arias <juan.fabian.arias@gmail.com>
2026-04-22 19:01:01 -0300

    clases e instancias terminadas

--- programacionDelJuego.js
+++ programacionDelJuego.js
@@ -50,7 +50,7 @@ class Turno{
 
     //metodo para recargar ki
     recarga(){
-        this._guerrero._ki += 100;
+        this._guerrero._ki += 10;
     }
 
     atacar(nombreAtaque, rival){
@@ -70,3 +70,53 @@ class Turno{
         console.log(`${this._guerrero._nombre} esta esquivando`);
     }
 }
+
+class Torneo{
+    constructor(guerrero1, guerrero2){
+        this._guerrero1 = guerrero1;
+        this._guerrero2 = guerrero2;
+        this._turnoActual = new Turno(this._guerrero1); 
+    }
+
+    cambiarTurno(){
+        if(this._turnoActual._guerrero === this._guerrero1){
+            this._turnoActual = new Turno(this._guerrero2);
+        }else{
+            this._turnoActual = new Turno(this._guerrero1);
+        }
+    }
+
+    verificarGanador() {
+    if(this._guerrero1._pts <= 0){
+        console.log(`Ganador: ${this._guerrero2._nombre}`);
+    } else if(this._guerrero2._pts <= 0){
+        console.log(`Ganador: ${this._guerrero1._nombre}`);
+    } else {
+        return 0;
+    }
+}
+}
+
+const bolKi = new Ataque ('Bolas de ki', 50, 15);
+const golpe = new Ataque ('Puño', 15, 0);
+const patada = new Ataque ('Patada', 30, 0);
+
+const guerreroA1 = new Guerrero ('Goku', 3000, 500, 100);
+const kamehame = new Ataque ('Kamehameha', 250, 30);
+guerreroA1.agregarAtaque(kamehame);
+guerreroA1.agregarAtaque(bolKi);
+guerreroA1.agregarAtaque(golpe);
+guerreroA1.agregarAtaque(patada);
+
+const guerreroA2 = new Guerrero ('Vegeta',2800, 500, 100);
+const galick = new Ataque ('Galick Gun', 250, 30);
+guerreroA2.agregarAtaque(galick);
+guerreroA2.agregarAtaque(bolKi);
+guerreroA2.agregarAtaque(golpe);
+guerreroA2.agregarAtaque(patada);
+
+const torneo = new Torneo (guerreroA1, guerreroA2);
+
+torneo._turnoActual.atacar("Kamehameha", guerreroA2);
+torneo.verificarGanador();
+torneo.cambiarTurno();
